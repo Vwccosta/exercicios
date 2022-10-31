797b91777e5e75e152427bfdcf68af2e15667c0d
Vander Wender <86972427+Vwccosta@users.noreply.github.com>
2022-10-31 09:01:30 -0300

    Create contadorDeClique.js

---
+++ contador de cliqu/contadorDeClique.js
@@ -0,0 +1,10 @@
+var contador = 0;
+function contadorDeCliques(){
+    contador ++
+    res = document.getElementById('showContador');
+    res.innerHTML = `<p>O numero de clique dado foi ${contador}</p>`
+}
+function zerarCliques(){
+    contador = 0;
+    res.innerHTML = null;
+}
